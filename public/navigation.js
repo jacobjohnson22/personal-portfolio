47008534eefa2a706a40c1f1afaaf026768763bf
Jacob <jacobjohnson0422@gmail.com>
2026-01-22 23:37:22 -0600

    updated

--- public/navigation.js
+++ public/navigation.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <div id="nav-grid" class="flex flex-row px-4 py-2 sm:px-8">
           <div id="nav-logo">
             <li>
-              <a href="index.html">
+              <a href=".">
                 <img src="/JJ logo.png" alt="Logo" class="w-8 md:w-10 xl:w-11">
               </a>
             </li>
@@ -16,10 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
           <div id="nav-links" class="flex gap-4 justify-end w-full items-center">
             <li class="text-sm px-2.5 py-1 rounded-lg main-transition hover:bg-(--hover-gray) md:text-base xl:text-lg">
-              <a href="index.html">Home</a>
+              <a href=".">Home</a>
             </li>
             <li class="text-sm px-2.5 py-1 rounded-lg main-transition hover:bg-(--hover-gray) md:text-base xl:text-lg">
-              <a href="/projects.html">Projects</a>
+              <a href=".">Projects</a>
             </li>
             <li class="text-sm px-2.5 py-1 rounded-lg main-transition hover:bg-(--hover-gray) md:text-base xl:text-lg">
               <a href="#contact-container">Contact Me</a>
