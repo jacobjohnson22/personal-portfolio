8e7c7b541d01c17f3096d52f9b59aaec50fb49fd
Jacob <jacobjohnson0422@gmail.com>
2026-01-22 22:22:49 -0600

    Updated

--- src/navigation.js
+++ src/navigation.js
@@ -3,7 +3,7 @@ document.getElementById("nav-container").innerHTML = `
         <ul>
             <div id="nav-grid" class="flex flex-row px-4 py-2 sm:px-8">
             <div id="nav-logo">
-                <li><a href="index.html"><img src="src/JJ logo.png" alt="Logo" class="w-8 md:w-10 xl:w-11"></a></li>
+                <li><a href="index.html"><img src="/JJ logo.png" alt="Logo" class="w-8 md:w-10 xl:w-11"></a></li>
             </div>
             
             <div id="nav-links" class="flex gap-4 justify-end w-full items-center">
